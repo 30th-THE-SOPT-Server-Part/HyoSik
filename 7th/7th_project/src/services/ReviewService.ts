6dca1b026073256ab2f603e6534769950af98619
Hyosik Philip Joo <lux1937@gmail.com>
2022-06-10 22:43:02 +0900

    [FEAT] Implement Get Reivew by keyword, option, page

--- 7th/7th_project/src/services/ReviewService.ts
+++ 7th/7th_project/src/services/ReviewService.ts
@@ -2,6 +2,9 @@ import { PostBaseResponseDto } from "../interfaces/common/PostBaseResponseDto";
 import { ReviewCreateDto } from "../interfaces/review/ReviewCreateDto";
 import { ReviewResponseDto } from "../interfaces/review/ReviewResponseDto";
 import Review from "../models/Review";
+import { ReviewInfo } from "../interfaces/review/ReviewInfo";
+import { ReviewOptionType } from "../interfaces/review/ReviewOptionType";
+import { ReviewsResponseDto } from "../interfaces/review/ReviewsResponseDto";
 
 const createReview = async (movieId: string, reviewCreateDto: ReviewCreateDto): Promise<PostBaseResponseDto> => {
     try {
@@ -25,27 +28,55 @@ const createReview = async (movieId: string, reviewCreateDto: ReviewCreateDto): 
     };
 };
 
-const getReviews = async (movieId: string): Promise<ReviewResponseDto[]> => {
+const getReviews = async (movieId: string, search: string, option: ReviewOptionType, page: number): Promise<ReviewsResponseDto> => {
+    const regex = (pattern: string) => new RegExp(`.*${pattern}.*`);
+
+    let reviews: ReviewInfo[] = [];
+    const perPage: number = 2;
+
     try {
-        const reviews = await Review.find({
-            movie: movieId
-        }).populate('writer', 'name').populate('movie');
+        const pattern: RegExp = regex(search);
 
-        const data = await Promise.all(reviews.map((review: any) => {
-            const result = {
-                writer: review.writer.name,
-                movie: review.movie,
-                title: review.title,
-                content: review.content
-            };
-            return result;
-        }));
+        if (option === 'title') {
+            reviews = await Review.find({ title: { $regex: pattern } })
+                .where('movie').equals(movieId)
+                .populate('writer')
+                .sort({ createdAt: -1 })
+                .skip(perPage * (page - 1))
+                .limit(perPage);
+        } else if (option === 'content') {
+            reviews = await Review.find({ content: { $regex: pattern } })
+                .where('movie').equals(movieId)
+                .populate('writer')
+                .sort({ createdAt: -1 })
+                .skip(perPage * (page - 1))
+                .limit(perPage);
+        } else {
+            reviews = await Review.find({
+                $or: [
+                    { title: { $regex: pattern } },
+                    { content: { $regex: pattern } }
+                ]
+            })
+            .where('movie').equals(movieId)
+            .populate('writer')
+            .sort({ createdAt: -1 })
+            .skip(perPage * (page - 1))
+            .limit(perPage);
+        }
 
+        const total = await Review.countDocuments({});
+        const lastPage: number = Math.ceil(total / perPage);
+
+        const data = {
+            reviews,
+            lastPage
+        }
         return data;
     } catch (error) {
         console.log(error);
         throw error;
-    };
-};
+    }
+}
 
 export default { createReview, getReviews };
