2544b1fec771e859bc38b8b82526500a1412421f
Hyosik Philip Joo <lux1937@gmail.com>
2022-06-10 19:22:32 +0900

    [FEAT] Implement File Service

--- 6th/6th_project/src/services/index.ts
+++ 6th/6th_project/src/services/index.ts
@@ -3,9 +3,11 @@
 import UserService from "./UserService";
 import ReviewService from "./ReviewService";
 import MovieService from "./MovieService";
+import FileService from "./FileService";
 
 export {
     UserService,
     ReviewService,
-    MovieService
+    MovieService,
+    FileService
 }
