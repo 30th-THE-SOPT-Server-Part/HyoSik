976f87389faa667228166b8817ed527ce4904b8a
Hyosik Philip Joo <lux1937@gmail.com>
2022-06-10 19:22:46 +0900

    [FEAT] Implement File Router

--- 6th/6th_project/src/routes/index.ts
+++ 6th/6th_project/src/routes/index.ts
@@ -3,12 +3,13 @@ import { Router } from 'express';
 import UserRouter from "./UserRouter";
 import ReviewRouter from "./ReviewRouter";
 import MovieRouter from "./MovieRouter";
-
+import FileRouter from "./FileRouter";
 
 const router: Router = Router();
 
 router.use('/user', UserRouter);
 router.use('/review', ReviewRouter);
 router.use('/movie', MovieRouter);
+router.use('/file', FileRouter);
 
 export default router;
